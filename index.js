40995e3d1b07feb9071dd0c593a16f70ac749813
pakki237 <adish3518@gmail.com>
2026-01-18 01:15:16 +0530

    index

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /* Move to category window */
 function goCategory() {
-  document.body.className = "state-category";
+    document.body.className = "state-category";
 }
 
 /* Move to signup choice */
 function goSignupChoice() {
-  document.body.className = "state-signup-choice";
+    document.body.className = "state-signup-choice";
 }
 
 /* Move to final signup + slideshow */
 function goFinalSignup() {
-  document.body.className = "state-signup-final";
-  startSlideshow();
+    document.body.className = "state-signup-final";
+    startSlideshow();
 }
 
 /* -----------------------
@@ -22,27 +22,33 @@ let slideIndex = 0;
 let slideTimer = null;
 
 function startSlideshow() {
-  stopSlideshow();
-  showSlide(slideIndex);
-  slideTimer = setInterval(() => nextSlide(), 5000);
+    stopSlideshow();
+    showSlide(slideIndex);
+    slideTimer = setInterval(() => nextSlide(), 5000);
 }
 
 function stopSlideshow() {
-  if (slideTimer) clearInterval(slideTimer);
+    if (slideTimer) clearInterval(slideTimer);
 }
 
 function showSlide(index) {
-  const slides = document.getElementsByClassName("slide");
-  slideIndex = (index + slides.length) % slides.length;
+    const slides = document.getElementsByClassName("slide");
+    slideIndex = (index + slides.length) % slides.length;
 
-  for (let s of slides) s.style.display = "none";
-  slides[slideIndex].style.display = "block";
+    for (let s of slides) s.style.display = "none";
+    slides[slideIndex].style.display = "block";
 }
 
 function nextSlide() {
-  showSlide(slideIndex + 1);
+    showSlide(slideIndex + 1);
 }
 
 function plusSlides(n) {
-  showSlide(slideIndex + n);
+    showSlide(slideIndex + n);
+}
+
+function startSlideshow() {
+    stopSlideshow();
+    showSlide(slideIndex);
+    slideTimer = setInterval(() => nextSlide(), 5000);
 }
